5e80d5a616174052416e83885a6cbf8efeeeb434
EDUARDO ANGELI <eduardo_angeli@estudante.sesisenai.org.br>
2024-04-03 21:56:59 -0300

    Correção 1

--- app/php/conexao.php
+++ app/php/conexao.php
@@ -1,5 +1,6 @@
 <?php 
 
-    $conexao = mysql_connect("localhost", "root", "", "nordicfinance") or die("Erro ao tentar se conectar com o banco" .mysql_connect_error());
+    //Conexão feita com o banco
+    $conexao = mysqli_connect("localhost","root","","nordicfinance") or die ("Erro ao tentar se conectar com o banco" . mysqli_connect_error());
 
 ?>
--- app/php/validaLogin.php
+++ app/php/validaLogin.php
@@ -1,28 +1,44 @@
 <?php 
+
+    if(session_status() !== PHP_SESSION_ACTIVE){
+        session_start();
+    }
+
+    $_SESSION['logado'] = 0;
+
+    //Recebe os valores Email e Senha da tela login por meio de post
     $email = $_POST["nEmail"];
     $senha = $_POST["nSenha"];
 
-    include(conexao.php);
-    
+    //Inclui a conexão com o banco php
+    include("conexao.php"); 
+
+    //Texto transcrito para o banco onde é feito o select com as informações obtidas
     $sql = "SELECT * FROM USUARIO WHERE EMAIL = '".$email."' AND SENHA = md5('".$senha."');";
+
+    //Resultado do texto feito anteriormente transportado para o banco por meio da conexao.
     $resultSql = mysql_query($conexao, $sql);
-    mysql_close(conexao);
 
-    if(mysql_num_rows(resultSql) > 0){
+    //Após pesquisa e resultado encerra a conexão com o banco
+    mysqli_close(conexao);
 
+    //Avalia se existe linhas no banco com os parâmetros inseridos
+    if(mysqli_num_rows(resultSql) > 0){
+
+        //Recebe o valor da linha selecionada a partir da execução e adicionada ao valor SESSION
         foreach ($resultSql as $call){
             $_SESSION['idTipoUsuario'] = $call['idTipoUsuario'];
             $_SESSION['logado'] = 1;
 
-            //Assim que for criada tela seguinte adicionar no lugar de "ProximaTela"
+            //Transfere o Usuário para a proxima tela
             //header('location: ../ProximaTela');
-
 
         }
 
+    //Caso não seja encontrado usuário com os parâmetros entregues ele retorna para a tela de login
     }else{
-        //Voltar para INDEX
-        header('location:..//');
+        
+        header('location:../');
 
     }
 
