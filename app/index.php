1f74905341f03a5fe4202edc102baa31dec85f16
guimdavila <davilaa754@gmail.com>
2024-04-03 21:50:36 -0300

    atualização

--- app/index.php
+++ app/index.php
@@ -23,9 +23,9 @@
 </head>
 
 <body>
-<<<<<<< HEAD
+
     <form method="POST" action="php/validaLogin.php">
-=======
+
     
 <img src="dist/img/logoNordicFinance.png" alt="">
 
@@ -43,7 +43,6 @@
             </div>
 
             <button type="submit" class="btn btn-primary">Entrar</button>
->>>>>>> 0e0af77109f29ffc9ebf3dfb19499e7774bb89f2
 
         </form>
 
