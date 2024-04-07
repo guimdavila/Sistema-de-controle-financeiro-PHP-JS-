24a08bbc1ebcab587fcf49afe5319759a88fd377
Guilherme Davila de Souza <davilaa754@gmail.com>
2024-04-07 17:18:23 -0300

    correção pagina empresa

---
+++ app/perfilUsuario.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Nordic Finance</title>
+
+  <?php
+  include('partes/css.php') //importes de CSS
+  ?>
+
+
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+
+    <?php
+    include('partes/navbar.php') //importes de CSS
+    ?>
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="index3.html" class="brand-link">
+        <img src="dist/img/logo128x128.png" alt="Nordic System" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Nordic System</span>
+      </a>
+
+
+      <?php
+      include('partes/sidebar.php') //importes de CSS
+      ?>
+
+    </aside>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+
+  <?php
+  include('partes/js.php') //importes de CSS
+  ?>
+
+</body>
+
+</html>
