83a7c2bb3f01970e00afade79039242e37136362
EDUARDO ANGELI <eduardo_angeli@estudante.sesisenai.org.br>
2024-04-15 20:02:46 -0300

    Adicionado validação de senha para alterar usuário

--- app/php/criarConta.php
+++ app/php/criarConta.php
@@ -1,37 +1,66 @@
 <?php 
-    if(session_status() !== PHP_SESSION_ACTIVE){
-        session_start();
+
+    function criaUser(){
+        if(session_status() !== PHP_SESSION_ACTIVE){
+            session_start();
+        }
+
+        $nome       = $_POST['nNomeUser'];
+        $email      = $_POST['nEmail'];
+        $senha      = $_POST['nSenha'];
+        $confSenha  = $_POST['nConfirmaSenha'];
+
+        if($senha != $confSenha){
+            header('location: ../criarUsuario.php');       
+
+        }
+
+        include("conexao.php");
+
+        $sql = "SELECT EMAIL FROM USUARIO WHERE EMAIL = '". $email ."'";
+
+        $resultSql = mysqli_query($conexao, $sql);
+
+        if(mysqli_num_rows($resultSql) > 0){
+            header('location: ../criarUsuario.php');        
+
+        }else{
+            $sql = "INSERT INTO USUARIO(NOMEUSUARIO, EMAIL, SENHA, IDTIPOUSUARIO) "
+            ."VALUES('".$nome."', '".$email."', MD5('".$senha."'), 1)";       
+
+            $criaConta = mysqli_query($conexao, $sql);
+
+            header('location: ../');
+
+        }
+
+        mysqli_close($conexao);
     }
 
-    $nome       = $_POST['nNomeUser'];
-    $email      = $_POST['nEmail'];
-    $senha      = $_POST['nSenha'];
-    $confSenha  = $_POST['nConfirmaSenha'];
+    function validaUser(){
 
-    if($senha != $confSenha){
-        header('location: ../criarUsuario.php');       
+        //Recebe os valores entregas por meio da função post do html
+        $email      = $_POST['nEmail'];
+        $senha      = $_POST['nSenha'];
 
-    }
+        include ("conexao.php");
 
-    include("conexao.php");
+        $sqlSenha = "SELECT IDUSUARIO FROM USUARIO WHERE EMAIL = '".$email."' and senha = '".$senha."'";
 
-    $sql = "SELECT EMAIL FROM USUARIO WHERE EMAIL = '". $email ."'";
+        $resultadoSql = mysqli_query($conexao,$sqlSenha);
 
-    $resultSql = mysqli_query($conexao, $sql);
+        //Valida se a senha inserida confere com a do usuario
+        if(mysqli_num_rows($resultadoSql) > 0){
+            header('location: ../criarUsuario.php');       
 
-    if(mysqli_num_rows($resultSql) > 0){
-        header('location: ../criarUsuario.php');        
+        }
 
-    }else{
-        $sql = "INSERT INTO USUARIO(NOMEUSUARIO, EMAIL, SENHA, IDTIPOUSUARIO) "
-        ."VALUES('".$nome."', '".$email."', MD5('".$senha."'), 1)";       
+        //Inicia a conexão com o banco
+        
 
-        $criaConta = mysqli_query($conexao, $sql);
 
-        header('location: ../');
 
     }
 
-    mysqli_close($conexao);
 
 ?>
