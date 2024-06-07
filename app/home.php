46bacdc9d6865bbc4709a31d087c46544c5cfbdf
MATHEUS LEANDRO DE BORBA <matheusleandro221@gmail.com>
2024-06-06 22:01:34 -0300

    tela home

--- app/home.php
+++ app/home.php
@@ -57,9 +57,9 @@ include('partes/css.php'); //importes de CSS
                   <div class="col-6">
                     <div class="small-box bg-success">
                       <div class="inner">
-                        <h3>53<sup style="font-size: 20px"></sup></h3>
+                        <h3><?php echo qtdCategorias(); ?></h3>
 
-                        <p>Entradas</p>
+                        <p>Quantidade de Categorias</p>
                       </div>
                       <div class="icon">
                         <i class="ion ion-stats-bars"></i>
@@ -78,6 +78,46 @@ include('partes/css.php'); //importes de CSS
                       </div>
                     </div>
                   </div>
+
+
+                  <!-- PIE CHART -->
+
+                  <div class="col-6">
+                    <div class="card card-danger">
+                      <div class="card-header">
+                        <h3 class="card-title">Pie Chart</h3>
+
+                        <div class="card-tools">
+                          
+                         
+                        </div>
+                      </div>
+                      <div class="card-body">
+                        <canvas id="pieChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                      </div>
+                      <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                  </div>
+
+
+
+                  <div class="col-6">
+                    <!-- BAR CHART -->
+                    <div class="card card-success">
+                      <div class="card-header">
+                        <h3 class="card-title">Bar Chart</h3>
+                      </div>
+                      <div class="card-body">
+                        <div class="chart">
+                          <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                        </div>
+                      </div>
+                      <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                  </div>
+
                 </div>
               </div>
             </div>
@@ -91,6 +131,91 @@ include('partes/css.php'); //importes de CSS
   </div>
   <!-- ./wrapper -->
 
+  <script>
+    $(function() {
+
+      //-------------
+      //- PIE CHART -
+      //-------------
+      // Get context with jQuery - using jQuery's .get() method.
+      var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+      var pieData = {
+        labels: [
+          'Entradas',
+          'Saidas',
+        ],
+        datasets: [{
+          data: [700, 500],
+          backgroundColor: ['#f56954', '#00a65a'],
+        }]
+      }
+      var pieOptions = {
+        maintainAspectRatio: false,
+        responsive: true,
+      }
+      //Create pie or douhnut chart
+      // You can switch between pie and douhnut using the method below.
+      new Chart(pieChartCanvas, {
+        type: 'pie',
+        data: pieData,
+        options: pieOptions
+      })
+
+
+      var areaChartData = {
+        labels: ['Admin', 'Empresa', 'Comum'],//CRIAR UM ECHO EM PHP PARA RETORNAR AS LABELS
+        datasets: [
+
+          {
+            label: 'Ativo',
+            backgroundColor: 'rgba(60,141,188,0.9)',
+            borderColor: 'rgba(60,141,188,0.8)',
+            pointRadius: false,
+            pointColor: '#3b8bba',
+            pointStrokeColor: 'rgba(60,141,188,1)',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(60,141,188,1)',
+            data: [28, 48, 40] //CRIAR UM ECHO EM PHP PARA RETORNAR OS RESULTAODS
+          },
+
+          {
+            label: 'Inativo',
+            backgroundColor: 'rgba(210, 214, 222, 1)',
+            borderColor: 'rgba(210, 214, 222, 1)',
+            pointRadius: false,
+            pointColor: 'rgba(210, 214, 222, 1)',
+            pointStrokeColor: '#c1c7d1',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(220,220,220,1)',
+            data: [65, 59, 80]
+          },
+
+        ]
+      }
+      //-------------
+      //- BAR CHART -
+      //-------------
+      var barChartCanvas = $('#barChart').get(0).getContext('2d')
+      var barChartData = $.extend(true, {}, areaChartData)
+      var temp0 = areaChartData.datasets[0]
+      var temp1 = areaChartData.datasets[1]
+      barChartData.datasets[0] = temp0
+      barChartData.datasets[1] = temp1
+
+      var barChartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        datasetFill: false
+      }
+
+      new Chart(barChartCanvas, {
+        type: 'bar',
+        data: barChartData,
+        options: barChartOptions
+      })
+
+    })
+  </script>
 
 </body>
 
